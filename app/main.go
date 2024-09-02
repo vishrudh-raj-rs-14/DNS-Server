37f7aad42f7767c5292b1011e44cf231d50b5129
vishrudh raj <vishrudh.shrinivas@gmail.com>
2024-09-02 19:57:17 +0530

    test

--- app/main.go
+++ app/main.go
@@ -65,7 +65,7 @@ func main() {
 			dnsQueryCopy.Header.QDCount = 1;
 			dnsQueryCopy.Header.ANCount = 0;
 			dnsQueryCopy.Header.ID = uint16(int(dnsQueryCopy.Header.ID))
-			fmt.Println(dnsQueryCopy.Question[0].Question)
+			fmt.Println(len(dnsQueryCopy.Question))
 			req := dnsQueryCopy.ParseMsg()
 			_, err := conn.Write(req);
 			if(err!=nil){
